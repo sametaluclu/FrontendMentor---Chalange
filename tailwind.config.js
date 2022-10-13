6a3ddd1655b6f2daf38582e916875e67d45d49de
Samet ALUÇLU <samet.aluclu.44@gmail.com>
2022-10-13 10:32:02 +0300

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         "b-white" : "hsl(0, 0%, 100%)",
         "text-blue" : "hsl(220, 15%, 55%)",
         "min-gray" : "hsl(218, 44%, 22%)",
+        "vry-dark-blue" : "hsl(217, 54%, 11%)",
+        "card-blue" : "hsl(216, 50%, 16%)",
+        "line-blue" : "hsl(215, 32%, 27%)",
       }
     },
   },
